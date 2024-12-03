597ae3ee8aa411e777f28d4a2c161180b6b59d5d
Frejus DASSI <frejohope@gmail.com>
2024-12-03 18:20:25 +0100

    Mettons à jour la store

--- chirper/app/Http/Controllers/ChirpController.php
+++ chirper/app/Http/Controllers/ChirpController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 // use Illuminate\Http\Response;
 use App\Models\Chirp;
+use Illuminate\Http\RedirectResponse;
 use Inertia\Inertia;
 use Inertia\Response;
 use Illuminate\Http\Request;
@@ -34,9 +35,16 @@ class ChirpController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
         //
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+ 
+        $request->user()->chirps()->create($validated);
+ 
+        return redirect(route('chirps.index'));
     }
 
     /**
